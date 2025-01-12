2780ebdb4ac2f572371225a52b16d3dcdcd1e453
Chris Mathew Aje <chrismaje63@gmail.com>
2025-01-12 18:40:59 +0530

    Update script.js web app url

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ form.addEventListener('submit', async (e) => {
 // Submit form data to Google Sheets
 async function submitForm(data) {
   try {
-    const response = await fetch(https://script.google.com/macros/s/AKfycbzs8SkcKsZa9iv4mLK9tIERNkSJu0dix9IlNcZU4-rxCngIqi3dNQzvMTQ3WFSKfBU_/exec, {
+    const response = await fetch('https://script.google.com/macros/s/AKfycbzI4KTsi9c00UeAl4HgnD8YUBw3Puacscwwnm0x-BF3c1pG4QwCN0jyxEF2Q8YzDzEz/exec', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
